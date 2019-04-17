9a14d0696bc82002648d281fad8d32e7bc915cab
mc-santiago <mari.c.santiago@europe.com>
2019-04-17 10:30:51 +0200

    before quest  3 changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import Quote from "./Quote";
+//import Quote from "./Quote";
+import Quotes from "./Quotes";
 
 class App extends Component {
+  
+
   render() {
     return (
-      <div className="App">
+      <div className="A6pp">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Quote
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
+        <Quotes/>
       </div>
     );
   }
